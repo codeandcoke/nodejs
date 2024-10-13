20799e9f22a677ed4c2a82620cccbb57801f0af7
Santiago Faci <santi@codeandcoke.com>
2024-10-13 21:07:31 +0200

    API sencilla solo backend

---
+++ cities-backend/sample1.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const cities = {
+    'Zaragoza': {
+        altitude: 199,
+        population: 673010
+    },
+    'Huesca': {
+        altitude: 488,
+        population: 53305
+    },
+    'Teruel': {
+        altitude: 915,
+        population: 25900
+    }
+};
+
+app.get('/cities', (req, res) => {
+    res.json(cities);
+});
+
+app.get('/city/:city', (req, res) => {
+    const city = req.params.city;
+    res.json(cities[city]);
+});
+
+
+app.listen(8080, () => {
+    console.log('Iniciando el backend en el puerto 8080');
+});
