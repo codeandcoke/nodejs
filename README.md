32d0bea90d80cff8b5135f30e85684d3344b6fe5
Santiago Faci <santi@codeandcoke.com>
2024-10-14 11:27:58 +0200

    Añadiendo nuevo ejemplo con algunas operaciones CRUD

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Repositorio con ejemplos de proyectos Node.js
 
 - **cities-backend**: API sencilla hecha con nodejs. Sólo backend
 - **cities-backend-frontend**: API sencilla hecha con nodejs. Incluye un frontend HTML + Javascript con llamadas al backend usando axios
+- **cities-backend-frontend-crud**: API sencilla hecha con nodejs con operación de registro. Incluye un frontend HTML + Javascript con llamadas al backend usando axios
--- cities-backend-frontend/frontend/src/app.js
+++ cities-backend-frontend/frontend/src/app.js
@@ -6,17 +6,8 @@ function addCityNode(name) {
     const item = document.createElement('li');
     item.className = 'list-group-item';
     item.appendChild(document.createTextNode(name));
-
-    const button = document.createElement('button');
-    button.className = 'btn-close'
-    button.onclick = function() {
-        removeCity(name);
-        item.remove();
-    };
-    item.appendChild(button);
-
     citiesUl.appendChild(item);
-}
+};
 
 window.readCities = function() {
     axios.get('http://localhost:8080/cities')
@@ -27,28 +18,4 @@ window.readCities = function() {
                 addCityNode(cityName);
             });
         });
-};
-
-window.addCity = function() {
-    const name = document.getElementById('name').value;
-    const altitude = document.getElementById('altitude').value;
-    const population = document.getElementById('population').value;
-
-    if (name === '') {
-        alert('El nombre es un campo obligatorio');
-        return;
-    }
-
-    axios.post('http://localhost:8080/cities', {
-        name: name,
-        altitude: altitude,
-        population: population
-    }).then(() => {
-        addCityNode(name);
-    });
-};
-
-window.removeCity = function(name) {
-    console.log(name + ' was removed');
-    // TODO Remove the city
 };
