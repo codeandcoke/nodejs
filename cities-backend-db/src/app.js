351b17a5f043b64df98d29dad5c88f3a27c16aee
Santiago Faci <santi@codeandcoke.com>
2024-10-20 14:41:44 +0200

    Arreglos en cities-backend-db

--- cities-backend-db/src/app.js
+++ cities-backend-db/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import knex from 'knex';
 
+// Configuración de la base de datos: tipo, ubicación y otros parámetros
 const db = knex({
     client: 'sqlite3',
     connection: {
@@ -12,31 +13,48 @@ const db = knex({
 const app = express();
 app.use(express.json());
 
-const cities = {
-    'Zaragoza': {
-        altitude: 199,
-        population: 673010
-    },
-    'Huesca': {
-        altitude: 488,
-        population: 53305
-    },
-    'Teruel': {
-        altitude: 915,
-        population: 25900
-    }
-};
-
+// Operación que devuelve todas las ciudades de la base de datos
 app.get('/cities', async (req, res) => {
     const result = await db('cities').select('*');
-    res.json(result);
+
+    res.status(200).json(result);
 });
 
+// Operación que devuelve una ciudad determinada
 app.get('/cities/:city', async (req, res) => {
     const result = await db('cities').select('*').where({name: req.params.city}).first();
-    res.json(result);
+
+    res.status(200).json(result);
 });
 
+// Operación que registra una nueva ciudad en la base de datos
+app.post('/cities', async (req, res) => {
+    await db('cities').insert({
+        name: req.body.name,
+        population: req.body.population,
+        altitude: req.body.altitude
+    });
+
+    res.status(201).json({});
+});
+
+// Operación que modifica una ciudad en la base de datos
+app.put('/cities/:city', async (req, res) => {
+    await db('cities').where({ name: req.params.city }).update({
+        population: req.body.population,
+        altitude: req.body.altitude
+    });
+
+    res.status(204).json({});
+});
+
+// Operación que elimina una ciudad de la base de datos
+app.delete('/cities/:city', async (req, res) => {
+    const cityName = req.params.city;
+    await db('cities').del().where({name: cityName});
+
+    res.status(204).json({})
+});
 
 app.listen(8080, () => {
     console.log('Iniciando el backend en el puerto 8080');
